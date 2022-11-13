ac788f12756faf99ca8f41033128ecd6a852aeba
Marc Castro Trullá <marccastro02@gmail.com>
2022-11-13 22:38:15 +0100

    Functions to generate passwords

---
+++ main.js
@@ -0,0 +1,39 @@
+"user strict";
+const generate = document.getElementById("submit");
+const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+const upperCaseLetters = "ABCDEFGHIJKLMNOPQSRTUVWXYZ";
+const numbers = "0123456789";
+const specialCharacters = "~!@#$%^&*()_+=|";
+var newPassword = "";
+
+const generatePassword = (lower, upper, number, special) => {
+    let arrCharacters = [];
+    const lowerSelect = document.getElementById("lowerCase");
+    const upperSelect = document.getElementById("upperCase");
+    const numberSelect = document.getElementById("numbers");
+    const specialCharacterSelect = document.getElementById("specialCharacters");
+
+    if(lowerSelect.checked) arrCharacters.push(lower[Math.floor(Math.random() * lower.length)]);
+    if(upperSelect.checked) arrCharacters.push(upper[Math.floor(Math.random() * upper.length)]);
+    if(numberSelect.checked) arrCharacters.push(number[Math.floor(Math.random() * number.length)]);
+    if(specialCharacterSelect.checked) arrCharacters.push(special[Math.floor(Math.random() * special.length)]);
+    
+    return arrCharacters.length === 0 ? "" : arrCharacters[Math.floor(Math.random() * arrCharacters.length)];
+}
+
+generate.addEventListener("click", (e) => {
+    e.preventDefault();
+    const length = document.getElementById("length").value;
+    newPassword = "";
+    if(Number(length) <= 0) alert("Tienes que poner la longitud que sea mayor de 0.");
+    else {   
+        for(let i = 0;i < length;i++) {
+            newPassword += generatePassword(lowerCaseLetters, upperCaseLetters, numbers, specialCharacters);
+            if(newPassword === "") {
+                i = length;
+                alert("Tienes que escoger que contendra la contraseña.");
+            }
+        }
+        document.getElementById("password").value = Number(newPassword) === 0 ? "" :  newPassword;
+    }
+});
